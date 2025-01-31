e4285e4073c06cc9bc313f2c11028a4c0109a5f7
Kir <5420754+lastonoga@users.noreply.github.com>
2025-01-31 15:28:55 +0700

    feat: add new feature

--- packages/nexa-starter/index.js
+++ packages/nexa-starter/index.js
@@ -1,7 +1,7 @@
 import { launchNexa } from '@nexa-js/nexa-core';
 import { AuthMiddleware } from './app/auth.js';
 
-const STATIC_API_TOKEN = '123456';
+const STATIC_API_TOKEN = 'auth_token_example';
 
 launchNexa((app) => {
     AuthMiddleware(app, STATIC_API_TOKEN);
